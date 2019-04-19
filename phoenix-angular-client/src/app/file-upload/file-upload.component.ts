5081a677d43b561c951fcdd0f3c84897a52c3019
Aswathy Kanakarajan <aswathykanakarajan@zipcoders-MacBook-Pro-7.local>
2019-04-19 11:46:09 -0400

    Upload busy cursor

--- phoenix-angular-client/src/app/file-upload/file-upload.component.ts
+++ phoenix-angular-client/src/app/file-upload/file-upload.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { FileUploadService } from '../file-upload.service';
 
  const URL = 'http://localhost:3000/api/upload';
@@ -10,8 +10,10 @@ import { FileUploadService } from '../file-upload.service';
 export class FileUploadComponent implements OnInit {
   description : string;
   title : string;
+  @ViewChild('outer') htmlbody:ElementRef;
   constructor(private uploadService:FileUploadService) { 
-
+    
+    
   }
   @ViewChild("fileInput") fileInputVariable: any;
   ngOnInit() {
@@ -19,8 +21,15 @@ export class FileUploadComponent implements OnInit {
   }
   public upload(){
     console.log("upload ******")
+    document.body.classList.add('busy-cursor');
     const files = this.fileInputVariable.nativeElement.files;
     this.uploadService.upload(files[0], this.title,this.description)
-    .subscribe(something => {console.log(something)});
+    .subscribe(something => {
+      document.body.classList.remove('busy-cursor');
+      alert("Sucessfully uploaded your file to server");
+    },error => {
+      document.body.classList.remove('busy-cursor');
+      alert("An error occured while trying to uploaded, please try again!!");
+    });
   }
 }
